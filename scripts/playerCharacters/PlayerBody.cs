66e7419fff1e145d336701b8eacb6ab0abc3fe1a
Elena-Vidal <bernardorosa.bg015@academico.ifsul.edu.br>
2026-08-10 11:56:25 -0300

    Dodge Test v1

--- scripts/playerCharacters/PlayerBody.cs
+++ scripts/playerCharacters/PlayerBody.cs
@@ -1,12 +1,15 @@
 using Godot;
 using System;
+using System.Threading;
 
 public partial class PlayerBody : CharacterBody2D
 {
 	public const float Speed = 600.0f;
 
+
 	public override void _PhysicsProcess(double delta)
 	{
+		CollisionShape2D collision = GetNode<CollisionShape2D>("playerCollision");
 		Vector2 velocity = Velocity;
 
 		Vector2 direction = Input.GetVector("moveLeft", "moveRight", "moveUp", "moveDown");
@@ -34,6 +37,9 @@ public partial class PlayerBody : CharacterBody2D
 		if (Input.IsActionJustPressed("dodge"))
 		{
 			GD.Print("desvio");
+			collision.SetDeferred("DisableMode", true);
+			Thread.Sleep(1000);
+			collision.SetDeferred("DisabledMode", false);
 		}
 
 		Velocity = velocity;
