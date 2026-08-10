51f6ff92a78dfe6442a1ed5536ad94c84cf32a7b
Elena-Vidal <bernardorosa.bg015@academico.ifsul.edu.br>
2026-08-10 09:06:06 -0300

    Player basic movement

---
+++ scripts/playerCharacters/PlayerBody.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public partial class PlayerBody : CharacterBody2D
+{
+	public const float Speed = 600.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		Vector2 direction = Input.GetVector("moveLeft", "moveRight", "moveUp", "moveDown");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+		if (direction != Vector2.Zero)
+		{
+			velocity.Y = direction.Y * Speed;
+		}
+		else
+		{
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		}
+
+		if (Input.IsActionJustPressed("attack"))
+		{
+			GD.Print("ataque");
+		}
+		if (Input.IsActionJustPressed("dodge"))
+		{
+			GD.Print("desvio");
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
