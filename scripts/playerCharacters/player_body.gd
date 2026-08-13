c02c171f7b6b5788b2b5764f8ad9df1d3f018ed9
Elena-Vidal <bernardorosa.bg015@academico.ifsul.edu.br>
2026-08-13 08:51:19 -0300

    Dodge Test v4; code optimizations

--- scripts/playerCharacters/player_body.gd
+++ scripts/playerCharacters/player_body.gd
@@ -1,44 +1,27 @@
 extends CharacterBody2D
 
 
-const SPEED = 600.0
+const SPEED := 450.0
+const DODGE_SPEED := 700
+const DODGE_COOLDOWN := 0.8
+const DODGE_RANGE := 0.2
 
-var dodgeCooldown = true
-var lastDirection = Vector2(0, 0)
+var dodge_enabled : bool = true
+var dodge_range : float = 0
+var dodge_cooldown : float = 0
+var dodge_direction : int = 0
 
 func _physics_process(delta: float) -> void:
 
-	var directionX := Input.get_axis("moveLeft", "moveRight")
-	var directionY := Input.get_axis("moveUp", "moveDown")
-	if directionX:
-		if dodgeCooldown == false:
-			velocity.x = SPEED
-		else:
-			velocity.x = directionX * SPEED
-		#lastDirection.x = directionX
+	var movement_direction : Vector2 = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
+	
+	if movement_direction:
+		velocity = movement_direction * SPEED
 	else:
-		#lastDirection.x = 0
-		velocity.x = move_toward(velocity.x, 0, 60)
-	if directionY:
-		velocity.y = directionY * SPEED
-		#lastDirection.y = directionY
-	else:
-		lastDirection.y = 0
-		#velocity.y = move_toward(velocity.y, 0, 60)
-
-	if Input.is_action_just_pressed("dodge") and dodgeCooldown and velocity.x != 0 and velocity.y != 0:
-		dodge()
-	print(position)
-	print(sign(position))
-
+		velocity.x = move_toward(velocity.x, 0, 45)
+		velocity.y = move_toward(velocity.y, 0, 45)
+	
+	if Input.is_action_just_pressed("dodge"):
+		print("dodge")
+	
 	move_and_slide()
-
-func dodge():
-	dodgeCooldown = false
-	var timer:SceneTreeTimer = get_tree().create_timer(0.5)
-	var collision = get_node("playerCollision")
-	velocity *= 5
-	collision.set_deferred("disable_mode", true)
-	timer.timeout.connect(set.bind("dodgeCooldown", true))
-	velocity /= 5
-	collision.set_deferred("disabled_mode", false)
