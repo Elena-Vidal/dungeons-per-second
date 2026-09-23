51bfd41ea0c054b757e9a470eb0f4ec5e5edfd37
Elena-Vidal <bernardorosa.bg015@academico.ifsul.edu.br>
2026-09-23 10:11:01 -0300

    Special room generation

--- scripts/enviroment/worldGeneration/randomWalker.gd
+++ scripts/enviroment/worldGeneration/randomWalker.gd
@@ -16,27 +16,34 @@ func _process(delta: float) -> void:
 	pass
 
 func _print_layout(level: Array) -> void:
-	for i in level.size():
+	for i in level.size()-1:
 		print(level[i], '\n')
 
 
 func _special_rooms(level: Array, size: Vector2i) -> void:
-	var start := Vector2i(0, 0)
-	var end := Vector2i(0, 0)
+	var start := Vector2i(-1, -1)
+	var end := Vector2i(-1, -1)
 	var item : Array[Vector2i]
-	var location := Vector2i(0, 0)
+	#var location : Vector2i
+	var different : bool = false
 	
 	item.resize(_rng.randi_range(1, 3))
 	
-	while true:
-		location = Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y))
-		if location == start:
-			break
+	start = Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y))
 	end = Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y))
-	for i in item.size():
-		item.set(i, Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y)))
+	while start == end:
+		start = Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y))
+		end = Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y))
+	for i in item.size()-1:
+		item[i] = Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y))
+		while item[i] == item[i-1] or item[i] == start or item[i] == end:
+			item[i] = Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y))
 	
-	
+	level[start.x][start.y] = 1
+	level[end.x][end.y] = 2
+	for i in item.size()-1:
+		level[item[i].x][item[i].y] = 8
+
 func _initialize_level() -> void:
 	for x in _level_dimensions.x:
 		level_layout.append([])
