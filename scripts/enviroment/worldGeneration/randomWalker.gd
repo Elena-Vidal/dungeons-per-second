b8a8dd5c9da386573cc28b8e6644725c43978e36
Elena-Vidal <bernardorosa.bg015@academico.ifsul.edu.br>
2026-09-24 14:08:45 -0300

    branching paths; adjusted grid size

--- scripts/enviroment/worldGeneration/randomWalker.gd
+++ scripts/enviroment/worldGeneration/randomWalker.gd
@@ -1,23 +1,37 @@
 extends Node2D
 
-@export var _level_dimensions := Vector2i(4, 8)
+@export var _level_dimensions := Vector2i(8, 8)
 @export var _start_room := Vector2i(-1, -1)
-@export var _level_length : int = 7
+@export var _level_length : int = 10
+@export var _item_rooms : int = 3
+@export var _item_path_lenght := Vector2i(1, 4)
+
 var level_layout : Array
+var possible_branches : Array[Vector2i]
 
 var _rng = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	_initialize_level()
 	_gen_start()
-	_gen_path(_start_room, _level_length)
+	_gen_path(_start_room, _level_length, "R")
+	_gen_item_rooms()
 	_print_layout()
 
 
 func _process(delta: float) -> void:
 	pass
 
-func _gen_path(current: Vector2i, length: int) -> bool:
+func _gen_item_rooms() -> void:
+	var created_branches : int = 0
+	var candidate : Vector2i
+	while created_branches < _item_rooms and possible_branches.size():
+		candidate = possible_branches[_rng.randi_range(0, possible_branches.size() - 1)]
+		if _gen_path(candidate, _rng.randi_range(_item_path_lenght.x, _item_path_lenght.y), str(created_branches + 1)):
+			created_branches += 1
+		else:
+			possible_branches.erase(candidate)
+func _gen_path(current: Vector2i, length: int, type: String) -> bool:
 	if length == 0:
 		return true
 	var current_room := current
@@ -36,10 +50,13 @@ func _gen_path(current: Vector2i, length: int) -> bool:
 		and current_room.y + move_direction.y >= 0 and current_room.y + move_direction.y < _level_dimensions.y
 		and not level_layout[current_room.x + move_direction.x][current_room.y + move_direction.y]):
 			current_room += move_direction
-			level_layout[current_room.x][current_room.y] = length
-			if _gen_path(current_room, length - 1):
+			level_layout[current_room.x][current_room.y] = type + str(length)
+			if length > 1:
+				possible_branches.append(current_room)
+			if _gen_path(current_room, length - 1,  "R"):
 				return true
 			else:
+				possible_branches.erase(current_room)
 				level_layout[current_room.x][current_room.y] = 0
 				current_room -= move_direction
 		move_direction = Vector2i(move_direction.y, -move_direction.x)
@@ -49,16 +66,16 @@ func _print_layout() -> void:
 	var layout : String = ""
 	for y in range(_level_dimensions.y -1, -1, -1):
 		for x in _level_dimensions.x:
-			layout += "[" + str(level_layout[x][y]) + "]"
+			layout += '\t' + "[" + str(level_layout[x][y]) + "]" + '\t'
 		layout += '\n'
-	print_rich(layout)
+	$Label.text = layout
 
 func _gen_start() -> void:
 	if _start_room.x < 0 or _start_room.x >= _level_dimensions.x:
 		_start_room.x = _rng.randi_range(0, _level_dimensions.x - 1)
 	if _start_room.y < 0 or _start_room.y >= _level_dimensions.y:
 		_start_room.y = _rng.randi_range(0, _level_dimensions.y - 1)
-	level_layout[_start_room.x][_start_room.y] = "[color=green]S[/color]"
+	level_layout[_start_room.x][_start_room.y] = "S"
 
 func _initialize_level() -> void:
 	for x in _level_dimensions.x:
