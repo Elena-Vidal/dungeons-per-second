27c6df3905c3221502b154e0164e72880451392d
Elena-Vidal <bernardorosa.bg015@academico.ifsul.edu.br>
2026-09-24 09:58:30 -0300

    level path generation

--- scripts/enviroment/worldGeneration/randomWalker.gd
+++ scripts/enviroment/worldGeneration/randomWalker.gd
@@ -1,48 +1,64 @@
 extends Node2D
 
-@export var _level_dimensions : Vector2i = Vector2i(4, 8)
-@export var _start_room : Vector2i
+@export var _level_dimensions := Vector2i(4, 8)
+@export var _start_room := Vector2i(-1, -1)
+@export var _level_length : int = 7
 var level_layout : Array
 
 var _rng = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	_initialize_level()
-	_special_rooms(level_layout, _level_dimensions)
-	_print_layout(level_layout)
+	_gen_start()
+	_gen_path(_start_room, _level_length)
+	_print_layout()
 
 
 func _process(delta: float) -> void:
 	pass
 
-func _print_layout(level: Array) -> void:
-	for i in level.size()-1:
-		print(level[i], '\n')
+func _gen_path(current: Vector2i, length: int) -> bool:
+	if length == 0:
+		return true
+	var current_room := current
+	var move_direction : Vector2i
+	match _rng.randi_range(0, 3):
+		0:
+			move_direction = Vector2i.UP
+		1:
+			move_direction = Vector2i.LEFT
+		2:
+			move_direction = Vector2i.DOWN
+		3:
+			move_direction = Vector2i.RIGHT
+	for i in 4:
+		if (current_room.x + move_direction.x >= 0 and current_room.x + move_direction.x < _level_dimensions.x
+		and current_room.y + move_direction.y >= 0 and current_room.y + move_direction.y < _level_dimensions.y
+		and not level_layout[current_room.x + move_direction.x][current_room.y + move_direction.y]):
+			current_room += move_direction
+			level_layout[current_room.x][current_room.y] = length
+			if _gen_path(current_room, length - 1):
+				return true
+			else:
+				level_layout[current_room.x][current_room.y] = 0
+				current_room -= move_direction
+		move_direction = Vector2i(move_direction.y, -move_direction.x)
+	return false
 
+func _print_layout() -> void:
+	var layout : String = ""
+	for y in range(_level_dimensions.y -1, -1, -1):
+		for x in _level_dimensions.x:
+			layout += "[" + str(level_layout[x][y]) + "]"
+		layout += '\n'
+	print_rich(layout)
 
-func _special_rooms(level: Array, size: Vector2i) -> void:
-	var start := Vector2i(-1, -1)
-	var end := Vector2i(-1, -1)
-	var item : Array[Vector2i]
-	#var location : Vector2i
-	var different : bool = false
-	
-	item.resize(_rng.randi_range(1, 3))
-	
-	start = Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y))
-	end = Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y))
-	while start == end:
-		start = Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y))
-		end = Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y))
-	for i in item.size()-1:
-		item[i] = Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y))
-		while item[i] == item[i-1] or item[i] == start or item[i] == end:
-			item[i] = Vector2i(_rng.randi_range(0, size.x), _rng.randi_range(0, size.y))
-	
-	level[start.x][start.y] = 1
-	level[end.x][end.y] = 2
-	for i in item.size()-1:
-		level[item[i].x][item[i].y] = 8
+func _gen_start() -> void:
+	if _start_room.x < 0 or _start_room.x >= _level_dimensions.x:
+		_start_room.x = _rng.randi_range(0, _level_dimensions.x - 1)
+	if _start_room.y < 0 or _start_room.y >= _level_dimensions.y:
+		_start_room.y = _rng.randi_range(0, _level_dimensions.y - 1)
+	level_layout[_start_room.x][_start_room.y] = "[color=green]S[/color]"
 
 func _initialize_level() -> void:
 	for x in _level_dimensions.x:
